73dd037da833653dc7052cd64c7c893af4d18cb2
fahad <43818190+fsq2@users.noreply.github.com>
2022-08-20 02:09:05 +0300

    finish ex 1

--- lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/upload_artifact.py
+++ lesson-1-machine-learning-pipelines/exercises/exercise_1/starter/upload_artifact.py
@@ -14,9 +14,8 @@ def go(args):
     logger.info("Creating run exercise_1")
 
     # Create a W&B run in the project ``exercise_1``. Set the option ``job_type="upload_file"``:
-
     # YOUR CODE HERE
-
+    run = wandb.init(project="exercise_1", job_type="upload_file")
     # Create an instance of the class ``wandb.Artifact``. Use the ``artifact_name`` parameter to fill
     # the keyword ``name`` when constructing the wandb.Artifact class.
     # Use the parameters ``artifact_type`` and ``artifact_desc`` to fill respectively the keyword
@@ -24,11 +23,23 @@ def go(args):
     # HINT: you can use args.artifact_name to reference the parameter artifact_name
 
     # YOUR CODE HERE
+    
+    artifact = wandb.Artifact(
+                            name=args.artifact_name,
+                            type=args.artifact_type,
+                            description=args.artifact_description
+                            
+                            )
 
     # Attach the file provided as the parameter ``input_file`` to the artifact instance using
     # ``artifact.add_file``, and log the artifact to the run using ``run.log_artifact``.
 
     # YOUR CODE HERE
+    
+    artifact.add_file(args.input_file)
+
+    # Save the artifact version to W&B and mark it as the output of this run
+    run.log_artifact(artifact)    
 
 
 if __name__ == "__main__":
